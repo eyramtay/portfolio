d9f48bf9f3aa9afc08b7473d66c73eb36b438a8a
eyramtay <71682389+eyramtay@users.noreply.github.com>
2024-05-10 11:18:40 -0500

    implemented sidebar/nav button feature

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,35 @@
+import { Sidebar } from "../sidebar/Sidebar";
 import "./navbar.scss";
-
+import { motion } from "framer-motion";
 export const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar />
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          {" "}
+          Eyram Tay
+        </motion.span>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" />
+          </a>
+          <a href="#">
+            <img src="/linkedin.png" />
+          </a>
+          <a href="#">
+            <img src="/dribbble.png" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
---
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -0,0 +1,5 @@
+function ToggleButton({ setOpen }) {
+  return <button onClick={() => setOpen((prev) => !prev)}>Button</button>;
+}
+
+export default ToggleButton;
