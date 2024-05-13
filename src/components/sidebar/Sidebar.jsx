ca05efbf4615b7632a2cbbcbffc04217888f3710
eyramtay <71682389+eyramtay@users.noreply.github.com>
2024-05-13 11:59:39 -0500

    added staggered animation to sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,27 +4,26 @@ import Links from "./links/Links";
 import "./sidebar.scss";
 import ToggleButton from "./toggleButton/toggleButton";
 
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 20,
+    },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.3,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
 export const Sidebar = () => {
   const [open, setOpen] = useState(false);
-
-  const variants = {
-    open: {
-      clipPath: "circle(1200px at 50px 50px)",
-      transition: {
-        type: "spring",
-        stiffness: 20,
-      },
-    },
-    closed: {
-      clipPath: "circle(30px at 50px 50px)",
-      transition: {
-        delay: 0.5,
-        type: "spring",
-        stiffness: 400,
-        damping: 40,
-      },
-    },
-  };
 
   return (
     <motion.div className="sidebar" animate={open ? "open" : "closed"}>
--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,5 +1,42 @@
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
 function Links() {
-  return <div className="links">Links</div>;
+  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
+
+  return (
+    <motion.div className="links" variants={variants}>
+      {items.map((item) => (
+        <motion.a href={`#${item}`} key={item} variants={itemVariants}>
+          {item}
+        </motion.a>
+      ))}
+    </motion.div>
+  );
 }
 
 export default Links;
